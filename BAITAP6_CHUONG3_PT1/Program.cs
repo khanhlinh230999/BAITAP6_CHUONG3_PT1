5ac6d7410c878e2de2d1757597c09fafeaa7046d
Linh Nguyễn <linhyun23091999@gmail.com>
2020-04-22 17:45:59 +0700

    Add project files.

---
+++ BAITAP6_CHUONG3_PT1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BAITAP6_CHUONG3_PT1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
